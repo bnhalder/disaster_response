c5107d1eb423e9a10be3f398c72974f613a236c9
Biswanath Halder <bhalder@Biswanaths-MacBook-Pro.local>
2018-09-14 22:51:47 +0530

    Added code for ML pipeline. Changed ETL pipeline notebook

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,77 @@
 import sys
+import sqlite3
+import pandas as pd
+import pickle
+from sqlalchemy import create_engine
+
+import nltk
+nltk.download(['punkt', 'wordnet'])
+import re
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics import classification_report
 
 
 def load_data(database_filepath):
-    pass
+    #conn = create_engine('sqlite:///InsertDatabaseName.db')
+    #df = pd.read_sql_table('InsertTableName', conn)
+    conn = sqlite3.connect('InsertDatabaseName.db')
+    df = pd.read_sql("SELECT * FROM InsertTableName", conn)
+    X = df.message.values
+    labels = df.columns[4:]
+    y = df[labels].values
+    return X, y, labels
 
 
 def tokenize(text):
-    pass
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+    
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens   
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', RandomForestClassifier())
+    ])
+
+    parameters = {
+        'vect__ngram_range': ((1, 1), (1, 2)),
+        'vect__max_df': (0.5, 0.75, 1.0),
+        'vect__max_features': (None, 5000, 10000),
+        'tfidf__use_idf': (True, False),
+        'clf__n_estimators': [50, 100, 200],
+        'clf__min_samples_split': [2, 3, 4]
+    }
+    
+    grid_obj = GridSearchCV(pipeline, param_grid=parameters)
+    return grid_obj
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    y_pred = model.predict(X_test)
+    print(classification_report(y_train, y_pred, target_names=category_names))
 
 
 def save_model(model, model_filepath):
-    pass
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
